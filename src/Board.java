f6a1fc0e7454a749f75360904d40e9cee0e502fc
Unknown <bilidanek@gmail.com>
2019-02-25 14:07:37 +0100

    Init

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+public class Board extends JFrame {
+	
+	Board()
+	{
+		setTitle("Chess plagiat 0b");
+		setSize(300,200);
+		setLocationRelativeTo(null);
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+public class Main {
+	public static void main(String[]args)
+	{
+		Board board = new Board();
+		board.setVisible(true);
+	}
+}
